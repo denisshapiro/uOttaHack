82446f3a0a92de4215bcc91168df85476d5466f8
Karim Hurani <karimhurani@gmail.com>
2019-02-10 02:20:23 -0500

    added some marker shit

--- app/static/map.js
+++ app/static/map.js
@@ -1,34 +1,42 @@
 var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 16
-  });
 
-  var pos;
-    // Try HTML5 geolocation.
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 16
+    });
+    console.log('yeet')
+
+    var pos;
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
+        navigator.geolocation.getCurrentPosition(function(position) {
             pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
 
-        map.setCenter(pos);
+            map.setCenter(pos);
 
             //Position Marker
             var image = 'imgs/hereicon.png';
             let currentMarker = new google.maps.Marker({
-            position: pos,
-            map: map,
-            icon: image
+                position: pos,
+                map: map,
+                icon: image
             });
 
         });
     } else {
-        // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+
+    let marker = new google.maps.Marker({
+        position: new google.maps.LatLng(45.423831, -75.681608),
+        map: map,
+        icon: 'https://i.imgur.com/XCXF71h.png'
+    })
+    console.log(marker)
 }
-
-
