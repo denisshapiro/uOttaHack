162c7dac4e213ac7b6df47ef213eca36133c08c7
Karim Hurani <karimhurani@gmail.com>
2019-02-10 00:41:39 -0500

    fixed requirements.txt

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/sms")
-def hello():
-    return "Hello World!"
+from app import app
 
 if __name__ == "__main__":
     app.run(debug=True)
