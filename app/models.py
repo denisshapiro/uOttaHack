88c9b6b4ff1f3199bb5a1a947dbf022abcc214f1
denis shapiro <38511040+denisshapiro@users.noreply.github.com>
2019-03-06 14:53:28 -0500

    google geocoding

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-class User(db.Model):
+class User(db.Model): 
     id    = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False)
     car = db.relationship("Car", uselist=False, back_populates="user")
